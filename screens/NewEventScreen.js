ecbec91c418df6bf34dccb813d75ac62cb6cbea0
William Mori <wmori@ucsb.edu>
2023-11-24 22:46:42 -0800

    added maps and location permissions

--- screens/NewEventScreen.js
+++ screens/NewEventScreen.js
@@ -1,11 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import Colors from '../Colors';
 
 export default function NewEventScreen() {
+  const [deviceLatitude, setDeviceLatitude] = useState();
+  const [deviceLongitude, setDeviceLongitude] = useState();
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log("Please grant location permissions")
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log("Location:");
+      console.log(currentLocation);
+    }
+    getPermissions();
+  }, [])
+
   return (
     <View style={styles.container}>
-        <Text>NewEventScreen</Text>
+        {location && <MapView 
+          style={styles.map} 
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          }}
+        />}
     </View>
   );
 }
@@ -13,8 +44,11 @@ export default function NewEventScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: Colors.background,
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height / 2.2
+  }
 });
